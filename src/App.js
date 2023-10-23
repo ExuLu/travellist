9908c7163abdabcb105904be4b656d1c2178ed41
Alena Sharai <exultantislupus@gmail.com>
2023-10-23 14:01:30 +0400

    add form and control values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,105 @@
-import './index.css';
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
 
 function App() {
-  return;
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
 
+function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function addItem(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className='add-form' onSubmit={addItem}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>ADD</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item itemObj={item} key={item.id} />
+        ))}
+      </ul>
+      <div className='actions'>
+        <select>
+          <option>Filter by quantity</option>
+          <option></option>
+          <option></option>
+        </select>
+        <button>Clear list</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ itemObj }) {
+  const { quantity, description, packed } = itemObj;
+  return (
+    <li>
+      <input type='checkbox'></input>
+      <span style={packed ? { textDecoration: 'line-through' } : {}}>
+        {quantity} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>🧳 You have 4 items in your list.</em>
+    </footer>
+  );
+}
 
 export default App;
