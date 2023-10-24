e07a28c289e0884961ee6ac8ac240095619260d2
Alena Sharai <exultantislupus@gmail.com>
2023-10-24 16:59:51 +0400

    add sorting

--- src/App.js
+++ src/App.js
@@ -85,10 +85,23 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onCheck }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  if (sortBy === 'status')
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             itemObj={item}
             key={item.id}
@@ -97,11 +110,15 @@ function PackingList({ items, onDeleteItem, onCheck }) {
           />
         ))}
       </ul>
-      <div className='actions'>
+      <div
+        className='actions'
+        value={sortBy}
+        onChange={(e) => setSortBy(e.target.value)}
+      >
         <select>
-          <option>Sort by input order</option>
-          <option>Sort by description</option>
-          <option>Sort by packed status</option>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='status'>Sort by packed status</option>
         </select>
         <button>Clear list</button>
       </div>
